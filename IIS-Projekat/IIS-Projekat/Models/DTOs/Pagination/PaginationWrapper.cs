114d6900588046590e6bbbc89b27833820ced9a5
dandza7 <dane.m.12@gmail.com>
2023-03-20 17:44:56 +0100

    Added pagination classes

---
+++ IIS-Projekat/IIS-Projekat/Models/DTOs/Pagination/PaginationWrapper.cs
@@ -0,0 +1,13 @@
+﻿namespace IIS_Projekat.Models.DTOs.Pagination
+{
+    public class PaginationWrapper<T>
+    {
+        public List<T> Items { get; set; } = new List<T>();
+        public int TotalCount { get; set; }
+        public PaginationWrapper(List<T> items, int totalCount)
+        {
+            Items = items;
+            TotalCount = totalCount;
+        }
+    }
+}
