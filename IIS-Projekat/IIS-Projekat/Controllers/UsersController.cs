931f99c3c31abfa594c6274065183f555504fc19
dandza7 <dane.m.12@gmail.com>
2023-03-20 18:19:17 +0100

    Added pagination description

--- IIS-Projekat/IIS-Projekat/Controllers/UsersController.cs
+++ IIS-Projekat/IIS-Projekat/Controllers/UsersController.cs
@@ -17,8 +17,21 @@ namespace IIS_Projekat.Controllers
         }
 
         /// <summary>
-        /// [Admin] Gets all users
+        /// [Admin] Gets all users (with pagination, sorting and filtering optional)
         /// </summary>
+        /// <remarks>
+        /// Pagination constraints (Everything is case insenstive):
+        /// <br/>  > Page and PageSize must be greater than 0 (0 if you want all items at once)
+        /// <br/>  > Orders:
+        /// <br/>  >>> OrderField must be one of the following: ID, Email, Role
+        /// <br/>  >>> Ordering must be either ASC or DESC
+        /// <br/>  > Filters:
+        /// <br/>  >>> Property must be one of the following: ID (number), Email (string), Role (string)
+        /// <br/>  >>> Connectings: 0 - AND, 1 - OR (filters will be applied as intersection for AND and will be applied as union for OR)
+        /// <br/>  >>> FilterValues:
+        /// <br/>  >>>>> FilterValue has to be able to be converted to correct type
+        /// <br/>  >>>>> Operations: 0 - StringContains, 1 - StringEquals, 2 - NumberEquals, 3 - NumberLessThan, 4 - NumberGreaterThan
+        /// </remarks>
         /// <response code="200">Returns all users</response>
         [HttpPost(Name = "GetAllUsers")]
         [Authorize(Roles = "ADMIN")]
