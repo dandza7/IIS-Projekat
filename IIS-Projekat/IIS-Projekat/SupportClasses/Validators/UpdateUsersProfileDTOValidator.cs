d0735f4a3f8368d23d608afb8eac9308fb0dd73b
dandza7 <dane.m.12@gmail.com>
2023-04-11 18:50:16 +0200

    Added Validators for profile update

---
+++ IIS-Projekat/IIS-Projekat/SupportClasses/Validators/UpdateUsersProfileDTOValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using IIS_Projekat.Models.DTOs.UsersProfile;
+
+namespace IIS_Projekat.SupportClasses.Validators
+{
+    public class UpdateUsersProfileDTOValidator : AbstractValidator<UpdateUsersProfileDTO>
+    {
+        public UpdateUsersProfileDTOValidator()
+        {
+            RuleFor(p => p.UserId).NotNull();
+            RuleFor(p => p.Gender).Must(gender => gender == "Male" || gender == "Female").WithMessage("Gender must be either \"Male\" or \"Female\"");
+        }
+    }
+}
