7dae7732c9932fd736914d15a0ec8086a5f3c8cd
dandza7 <dane.m.12@gmail.com>
2023-05-10 17:36:33 +0200

    Added repositories of new models to unit of work

--- IIS-Projekat/IIS-Projekat/Repositories/IUnitOfWork.cs
+++ IIS-Projekat/IIS-Projekat/Repositories/IUnitOfWork.cs
@@ -9,6 +9,19 @@ namespace IIS_Projekat.Repositories
         IGenericRepository<Exercise> ExerciseRepository { get; }
         IGenericRepository<MuscleGroup> MuscleGroupRepository { get; }
         IGenericRepository<ExerciseMuscleGroup> ExerciseMuscleGroupRepository { get; }
+        IGenericRepository<Diagnosis> DiagnosisRepository { get; }
+        IGenericRepository<Nutrition> NutritionRepository { get; }
+        IGenericRepository<Allergy> AllergyRepository { get; }
+        IGenericRepository<Food> FoodRepository { get; }
+        IGenericRepository<NutritionShare> NutritionShareRepository { get; }
+        IGenericRepository<Recipe> RecipeRepository { get; }
+        IGenericRepository<FoodShare> FoodShareRepository { get; }
+        IGenericRepository<Injury> InjuryRepository { get; }
+        IGenericRepository<MedicalRecord> MedicalRecordRepository { get; }
+        IGenericRepository<Appointment> AppointmentRepository { get; }
+        IGenericRepository<Report> ReportRepository { get; }
+
+
         void SaveChanges();
     }
 }
