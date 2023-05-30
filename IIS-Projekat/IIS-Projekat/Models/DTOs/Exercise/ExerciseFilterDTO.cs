f9c0c9631f65febc50d3471166b970632c8e0d8d
Nikola <nikolavukic333@gmail.com>
2023-05-30 22:30:27 +0200

    "Added exercise filtering for client exercises"

---
+++ IIS-Projekat/IIS-Projekat/Models/DTOs/Exercise/ExerciseFilterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace IIS_Projekat.Models.DTOs.Exercise
+{
+    public class ExerciseFilterDTO
+    {
+        public string PrimaryMuscleGroup { get; set; } = string.Empty;
+        public string SecondarymuscleGroup { get; set; } = string.Empty;
+        public string ExerciseNature { get; set; } = "Both";
+    }
+}
